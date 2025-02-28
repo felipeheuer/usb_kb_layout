d2d90dd7ed35f127e2342cab529450dde8673d7e
felipeheuer <picolli@gmail.com>
2025-02-28 18:29:01 +0100

    add install script
    Signed-off-by: felipeheuer <picolli@gmail.com>

--- usb_kb_layout_wrapper.sh
+++ usb_kb_layout_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # this is a wrapper script so udev can call the main script non-blocking
-# gitlab.com/felipeheuer
+# https://github.com/felipeheuer/usb_kb_layout
 
 /opt/usb_kb_layout/usb_kb_layout.sh &
